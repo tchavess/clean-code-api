7d147bb94b93c55ee978bbb2de686b87ba180c62
Thiago Chaves <thiago.chaves@calindra.com.br>
2022-04-19 19:15:28 -0300

    refactor: move duplicated code to a helper method

---
+++ src/presentation/helpers/http-helper.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// eslint-disable-next-line @typescript-eslint/quotes
+import { HttpResponse } from "../protocols/http"
+
+export const badRequest = (error: Error): HttpResponse => ({
+    statusCode: 400,
+    body: error
+})
