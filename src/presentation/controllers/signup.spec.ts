dfe40946feeaffe6c999be8a664a515a2950ad05
Thiago Chaves <thiago.chaves@calindra.com.br>
2022-04-21 20:34:33 -0300

    test: .lintstagedrs.json

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -5,7 +5,7 @@ describe('SignUp Controller', () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
-        email: 'any_email@mail.com',
+        email: 'any_email@mail.co',
         password: 'any_password',
         passwordConfirmation: 'any_password'
       }
