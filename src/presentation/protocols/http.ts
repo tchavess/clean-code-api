98e5b61586bff73b41ad09ec7143bc8f6ef23300
Thiago Chaves <thiago.chaves@calindra.com.br>
2022-04-21 20:29:10 -0300

    test: test lint-staged config

--- src/presentation/protocols/http.ts
+++ src/presentation/protocols/http.ts
@@ -1,13 +1,13 @@
 export interface HttpResponse {
-    statusCode: number
-    body: any
+  statusCode: number
+  body: any
 }
 
 export interface HttpRequest {
-    body?: {
-        name?: string
-        email?: string
-        password?: string
-        passwordConfirmation?: string
-    }
+  body?: {
+    name?: string
+    email?: string
+    password?: string
+    passwordConfirmation?: string
+  }
 }
