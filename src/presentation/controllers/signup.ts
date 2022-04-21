7baff51609debab4379be08a68c9b791bffc08db
Thiago Chaves <thiago.chaves@calindra.com.br>
2022-04-21 20:39:03 -0300

    feat: ensure SignUpController return an error if no passwor confirmation is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,7 +4,7 @@ import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse | undefined {
-    const requiredFields = ['name', 'email', 'password']
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
       if (!httpRequest.body?.[field]) {
         return badRequest(new MissingParamError(field))
