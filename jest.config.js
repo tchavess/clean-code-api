8ba3578f52a0d9d0a01b2763a4f3ee40e5b56d76
Thiago Chaves <thiago.chaves@calindra.com.br>
2022-05-11 19:02:28 -0300

    feat: ensure requests parse body as json

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,8 @@
-export default {
+module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   testEnviroment: 'node',
-  preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts*': 'ts-jest'
   }
